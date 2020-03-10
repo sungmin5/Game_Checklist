c98f0fc6e3aba40e2b222e2a450a43643d88468e
sungmin5 <sungmin.sam5@gmail.com>
2020-03-09 21:47:09 -0700

    Implement clock and reset checkboxes

--- GameChecker/src/checklists/CheckList1.java
+++ GameChecker/src/checklists/CheckList1.java
@@ -7,6 +7,8 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import javax.swing.JCheckBox;
 import javax.swing.SwingConstants;
@@ -15,7 +17,8 @@ import javax.swing.JButton;
 public class CheckList1  {
 
 	private JFrame frame;
-
+	private JLabel lblClock;
+	private JCheckBox[] ckboxes = new JCheckBox[8];
 	/**
 	 * Launch the application.
 	 */
@@ -34,11 +37,69 @@ public class CheckList1  {
 
 	/**
 	 * Create the application.
+	 * @throws InterruptedException 
 	 */
 	public CheckList1() {
 		initialize();
+		showTime();
+		scheduledReset();
 	}
-
+	
+	/*
+	 * showTime(): show the current time.
+	 * */
+	public void showTime() {
+		new Thread() {
+			public void run() {
+				// loop forever
+				while(true) 
+	            {
+					// get current time. 
+	                Calendar cal = new GregorianCalendar();
+	                int hour = cal.get(Calendar.HOUR_OF_DAY);
+	                int min = cal.get(Calendar.MINUTE);
+	                int sec = cal.get(Calendar.SECOND);
+	                
+	                // format the string in military time
+	                String time = String.format("%02d",hour) + ":" + String.format("%02d",min) + ":" + String.format("%02d",sec);
+	                
+	                // show the current time
+	                lblClock.setText(time);
+	        
+	            } //end while
+			} //run
+		}.start();
+	}// showTime
+	
+	/*
+	 * scheduledReset(): Reset checkboxes every 6 seconds. 
+	 * */
+	public void scheduledReset() {
+		new Thread() {
+			public void run() {
+				try {
+					// pause for 6 seconds
+					Thread.sleep((long) (0.1*60*1000));
+					// reset checkboxes
+					resetCheckboxes();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}//run
+		}.start();
+	}//scheduledReset
+	
+	
+	/*
+	 * resetCheckboxes(): reset all checkboxes
+	 * */
+	public void resetCheckboxes() {
+		for(int i = 0; i < ckboxes.length ; i++) {
+			ckboxes[i].setSelected(false);
+		}
+		scheduledReset();
+	}// resetCheckboxes
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -55,7 +116,7 @@ public class CheckList1  {
 		lblNewLabel.setBounds(40, 27, 347, 52);
 		frame.getContentPane().add(lblNewLabel);
 		
-		JCheckBox[] ckboxes = new JCheckBox[8];
+		
 		ckboxes[0] = new JCheckBox("Complete Basic Tutorial");
 		ckboxes[0].setBounds(46, 114, 144, 23);
 		frame.getContentPane().add(ckboxes[0]);
@@ -102,6 +163,10 @@ public class CheckList1  {
 			}
 		});
 		frame.getContentPane().add(btnNewButton);
+		
+		lblClock = new JLabel("clock");
+		lblClock.setBounds(40, 348, 49, 14);
+		frame.getContentPane().add(lblClock);
 		
 		
 	}
